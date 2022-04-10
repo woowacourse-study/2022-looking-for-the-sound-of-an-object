3fa2f63921030a65f9370f04a745e3dc45c295df
이아현 <lah1203@naver.com>
2022-04-11 01:37:50 +0900

    refactor: 만들고 있는 음료에 대한 설명 삭제

--- src/js/Drink.js
+++ src/js/Drink.js
@@ -8,14 +8,10 @@ export default class Drink {
   }
 
   make() {
-    const basicTemplate = `
-      <div>${this.#name}이(가) 만들어지고 있습니다.</div>
-    `;
-
     const makingTemplate = this.#ingredients
       .map(ingredient => `<div>${ingredient}이(가) 나옵니다.</div>`)
       .join('');
 
-    return basicTemplate + makingTemplate;
+    return makingTemplate;
   }
 }
