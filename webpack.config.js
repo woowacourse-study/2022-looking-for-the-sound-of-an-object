cae05b7bf35901eb6936961669e18d61269d40b5
JASUN LEE <liswktjs@gmail.com>
2022-04-11 17:16:36 +0900

    design: html 태그 작성 + style작업

--- webpack.config.js
+++ webpack.config.js
@@ -21,14 +21,6 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
-        ],
       },
       {
         test: /\.css$/,
