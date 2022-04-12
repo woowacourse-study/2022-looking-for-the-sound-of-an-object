1dff14b2d4bd280656f4222a3a4f4edc53c48b5d
Soyi Jeon <soyi.dev@gmail.com>
2022-04-12 10:40:17 +0900

    [요구사항 1] 소피아 제출합니다. 😎 (#1)
    * chore: .gitignore 추가
    
    * chore: eslint, prettier 설치 및 세팅
    
    * style: 기본 template, css 작성
    
    * feat: 아메리카노 만들기 기능 구현
    
    * feat: 가이드 메시지 업데이트 기능 구현
    
    * refactor: index.html 필요 없는 부분 삭제
    
    * chore: eslint config 수정
    
    * refactor: 음료 가져가기 버튼 id 변경
    
    * feat: 아메리카노 버튼, 음료 가져가기 버튼 disable attribute 세팅 추가
    
    - 음료 주문 전, 혹은 음료를 가져간 후에는 아메리카노 버튼 활성화, 음료 가져가기 버튼 비활성화
    - 음료 주문 후에는 아메리카노 버튼 비활성화
    - 음료 완성 후에는 음료 가져가기 버튼 활성화
    
    * style: 전체적으로 색상 변경 및 색상 변수 추가, css 파일 이동

---
+++ src/VendingMachine.js
@@ -0,0 +1,49 @@
+import { $ } from './util.js';
+
+export default class VendingMachine {
+  constructor() {
+    this.$menuArea = $('.menu-area');
+    this.$purchaseAmericanoButton = $('#purchase-americano-button', this.$menuArea);
+    this.$dispenser = $('.dispenser');
+    this.$beverage = $('.beverage', this.$dispenser);
+    this.$guideText = $('.dispenser-guide-text', this.$dispenser);
+    this.$takeOutButton = $('#take-out-button', this.$dispenser);
+
+    this.$purchaseAmericanoButton.addEventListener('click', this.makeAmericano);
+    this.$takeOutButton.addEventListener('click', this.initialize);
+
+    this.initialize();
+  }
+
+  initialize = () => {
+    this.$beverage.innerHTML = '';
+    this.$purchaseAmericanoButton.removeAttribute("disabled");
+    this.$takeOutButton.setAttribute("disabled", "");
+    this.updateGuideText('아메리카노 버튼을 선택하세요.');
+  }
+
+  makeAmericano = () => {
+    this.$purchaseAmericanoButton.setAttribute("disabled", "");
+    this.updateGuideText('음료 준비중...');
+    this.prepareIngredient('컵')
+      .then(() => this.prepareIngredient('에스프레소'))
+      .then(() => this.prepareIngredient('뜨거운 물'))
+      .then(() => { 
+        this.updateGuideText('음료가 다 나왔습니다. 가져가세요!');
+        this.$takeOutButton.removeAttribute("disabled");
+      });
+  }
+  
+  prepareIngredient(ingredient) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        this.$beverage.insertAdjacentHTML('beforeend', `<li>${ingredient}</li>`);
+        resolve();
+      }, 1000);
+    });
+  }
+
+  updateGuideText(guideMessage) {
+    this.$guideText.innerText = guideMessage;
+  }
+}
