1ee084fa58c6a5e517d60183548524252407cb60
Soyi Jeon <soyi.dev@gmail.com>
2022-04-16 01:21:38 +0900

    feature : 메뉴 버튼 가격 정보 추가 및 고객 충전 금액 업데이트 구현

--- src/OrderTaker.js
+++ src/OrderTaker.js
@@ -5,23 +5,31 @@ import { ORDER_PROGRESS } from './constants.js';
 export default class OrderTaker {
   constructor(order) {
     this.order = order;
+    this.customerCharge = 0;
+
+    this.$customerChargeArea = $('#customer-charge-area');
+    this.$customerChargeForm = $('#customer-charge-form');
+    this.$customerCharge = $('#customer-charge');
 
     this.$menuButtonArea = $('#menu-button-area');
     this.$$menuButtons;
     this.$guideText = $('#guide-message-area');
     this.$beveragePickUpButton = $('#beverage-pick-up-button');
     this.initializeMenuButtons();
+    this.updateCustomerCharge();
 
     this.order.addSubscriber(this.updateOnOrderChange);
+    this.$customerChargeForm.addEventListener('submit', this.onSubmitCustomerChargeForm);
     this.$menuButtonArea.addEventListener('click', this.onClickMenuArea);
     this.$beveragePickUpButton.addEventListener('click', this.onClickBeveragePickUpButton);
   }
   
   initializeMenuButtons() {
     this.$menuButtonArea.insertAdjacentHTML('afterbegin', Object.keys(menus).map((menu) => `
-      <button name="${menu}" type="button">${menus[menu].name}</button>
+      <button name="${menu}" type="button" disabled>${menus[menu].name}<br >${menus[menu].price.toLocaleString()}원</button>
     `).join(''));
     this.$$menuButtons = $$('button', this.$menuButtonArea);
+    this.setAllMenuButtonDisable();
   }
 
   updateOnOrderChange = ({progress, orderedMenu}) => {
@@ -40,7 +48,7 @@ export default class OrderTaker {
   }
 
   updateOnOrderPending = () => {
-    this.setAllMenuButtonActive();
+    // this.setAllMenuButtonActive();
     this.$beveragePickUpButton.setAttribute("disabled", "");
     this.updateGuideText('원하는 음료를 선택하세요.');
   }
@@ -53,6 +61,12 @@ export default class OrderTaker {
   updateOnOrderComplete = (menu) => {
     this.$beveragePickUpButton.removeAttribute("disabled");
     this.updateGuideText(`${menus[menu].name} 나왔습니다. 😉`);
+  }
+
+  onSubmitCustomerChargeForm = (event) => {
+    event.preventDefault();
+    const chargeValue = Number($('#customer-charge-input', event.target).value);
+    console.log(chargeValue);
   }
     
   onClickMenuArea = (event) => {
@@ -78,6 +92,10 @@ export default class OrderTaker {
     this.$$menuButtons.forEach((menuButton) => {
       menuButton.removeAttribute("disabled");
     })
+  }
+
+  updateCustomerCharge() {
+    this.$customerCharge.textContent = this.customerCharge.toLocaleString();
   }
   
   updateGuideText(guideMessage) {
