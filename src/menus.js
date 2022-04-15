7283104988791d8cf02cf37131b2df5120f97a41
Soyi Jeon <soyi.dev@gmail.com>
2022-04-16 01:18:44 +0900

    feature: menu price 추가

--- src/menus.js
+++ src/menus.js
@@ -1,18 +1,22 @@
 const menus = {
   espresso: {
     name: '에스프레소',
+    price: 1000,
     ingredients: ['에스프레소']
   },
   americano : {
     name: '아메리카노',
+    price: 2000,
     ingredients: ['에스프레소', '뜨거운 물']
   },
   cafeLatte : {
     name: '카페라떼',
+    price: 3000,
     ingredients: ['우유', '에스프레소']
   },
   milk : {
     name: '우유',
+    price: 1000,
     ingredients: ['우유']
   }
 }
