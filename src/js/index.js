66ef31c720600f2e108802ba8a0c068abe1c7c87
이아현 <57928612+LAH1203@users.noreply.github.com>
2022-04-16 15:35:06 +0900

    [요구사항 3] 하리 제출합니다 😆 (#8)
    * refactor: 객체 프로퍼티 접근 방식 변경
    
    * docs: 요구사항 작성
    
    * feat: 에스프레소, 아메리카노, 카페라떼, 우유를 만드는 자판기 기능 구현
    
    * refactor: 메뉴 버튼 템플릿화
    
    * docs: 3단계 요구사항 작성
    
    * refactor: 원재료, 음료수 상수화
    
    * feat: 투입할 금액 마크업, 스타일 추가
    
    * feat: 금액 투입 기능 구현
    
    * feat: 음료수 구매 기능 구현
    
    * refactor: localStorage key 상수화
    
    * refactor: 각 음료수 가격 변경
    
    * refactor: 현재 투입 금액 한국식으로 표현
    
    * feat: 각 버튼 클릭 후 자동으로 입 금액 입력 input에 포커스하도록 추가
    
    * feat: 투입할 금액 유효성 검사 기능 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,94 @@
-import Drink from './Drink.js';
+import { drinks } from './constants/drink.js';
+import { KEY } from './constants/index.js';
 import { $, $$ } from './utils/dom.js';
+import { store } from './utils/store.js';
+import { validateChargeMoney } from './utils/validator.js';
 
 class DrinkMachine {
   constructor() {
-    this.ingredients = {
-      espresso: '에스프레소',
-      hotWater: '뜨거운 물',
-      milk: '우유',
-    };
-    this.drinks = {
-      espresso: new Drink('에스프레소', [this.ingredients.espresso]),
-      americano: new Drink('아메리카노', [
-        this.ingredients.espresso,
-        this.ingredients.hotWater,
-      ]),
-      cafelatte: new Drink('카페라떼', [
-        this.ingredients.milk,
-        this.ingredients.espresso,
-      ]),
-      milk: new Drink('우유', [this.ingredients.milk]),
-    };
-
+    this.$totalChargeInput = $('.charge-money__input');
+    this.$totalChargeMoney = $('.charge-money__total');
     this.$dispenser = $('.dispenser');
     this.$clearButton = $('.clear');
-    this.$menuButtons = $$('.menu__button');
+    this.$menu = $('.menu');
+    this.$menuButtons;
 
-    $('.menu').addEventListener('click', this.menuButtonClickEvent);
+    this.showMenuButton();
+    this.showTotalMoney();
+    this.$totalChargeInput.focus();
+
+    $('.charge-money__form').addEventListener('submit', this.chargeMoneyEvent);
+    this.$menu.addEventListener('click', this.menuButtonClickEvent);
     this.$clearButton.addEventListener('click', this.clearDispenser);
+  }
+
+  chargeMoneyEvent = e => {
+    e.preventDefault();
+
+    const inputMoney = e.target.elements[0].valueAsNumber;
+
+    try {
+      validateChargeMoney(inputMoney);
+    } catch ({ message }) {
+      alert(message);
+      this.$totalChargeInput.value = '';
+      return;
+    }
+
+    store.set(KEY.CHARGE_MONEY, store.get(KEY.CHARGE_MONEY) + inputMoney);
+
+    this.showTotalMoney();
+    this.showMenuButton();
+    this.$totalChargeInput.value = '';
+  };
+
+  showTotalMoney() {
+    const totalMoney = store.get(KEY.CHARGE_MONEY);
+    this.$totalChargeMoney.textContent = totalMoney
+      ? totalMoney.toLocaleString('ko-KR')
+      : 0;
+  }
+
+  showMenuButton() {
+    const totalMoney = store.get(KEY.CHARGE_MONEY);
+    const template = Object.keys(drinks)
+      .map(drink => {
+        const { name, price } = drinks[drink];
+        return `
+          <button
+            class="menu__button"
+            name="${drink}"
+            type="button"
+            title=${price}
+            ${price > totalMoney ? 'disabled' : ''}
+          >${name}</button>
+        `;
+      })
+      .join('');
+
+    this.$menu.replaceChildren();
+    this.$menu.insertAdjacentHTML('beforeend', template);
+
+    this.$menuButtons = $$('.menu__button');
   }
 
   menuButtonClickEvent = e => {
     if (this.$dispenser.children.length > 0) return;
 
+    store.set(KEY.CHARGE_MONEY, store.get(KEY.CHARGE_MONEY) - e.target.title);
+    this.showTotalMoney();
+    this.showMenuButton();
+
     e.target.classList.add('active');
 
     const template = `
       <div>컵이 나옵니다.</div>
-      ${this.drinks[e.target.name].make()}
+      ${drinks[e.target.name].make()}
       <div>완성되었습니다.</div>
     `;
 
     this.showMaking(template);
+    this.$totalChargeInput.focus();
   };
 
   showMaking(template) {
@@ -64,6 +112,7 @@ class DrinkMachine {
 
     this.$dispenser.replaceChildren();
     this.$clearButton.toggleAttribute('hidden');
+    this.$totalChargeInput.focus();
   };
 }
 
