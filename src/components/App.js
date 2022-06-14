b1e82f3bd3cfd5b7d2e20a0affff805be588ab02
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-06-14 16:36:54 +0900

    feat: 아메리카노 버튼 클릭 로직 구현
    버튼 클릭시, 일정한 시간에 따라 디스펜서에 아메리카노가 나온다.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,39 @@
+import { useState } from "react";
+
+const useHandleDispenser = (timeInterval) => {
+  const [dispenserAction, setDispenserAction] = useState([]);
+  const handleDispenser = async (ingredient) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        setDispenserAction((prev) => [...prev, ingredient]);
+        resolve();
+      }, timeInterval);
+    });
+  };
+
+  return { dispenserAction, setDispenserAction, handleDispenser };
+};
+
 function App() {
-  return <div className="App">ㅗㅑㅗㅑ</div>;
+  const { dispenserAction, handleDispenser, setDispenserAction } =
+    useHandleDispenser(500);
+  const handleClickAmericanoButton = async () => {
+    setDispenserAction([]);
+    await handleDispenser("컵");
+    await handleDispenser("에스프레소");
+    await handleDispenser("뜨거운 물");
+  };
+
+  return (
+    <div className="App">
+      <button type="button" onClick={handleClickAmericanoButton}>
+        아메리카노
+      </button>
+      {dispenserAction.map((action) => (
+        <p key={action}>{action}</p>
+      ))}
+    </div>
+  );
 }
 
 export default App;
