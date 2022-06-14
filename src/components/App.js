dfd73d1fee119f1e50fc386e44eb301e6a4c4988
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-06-14 16:53:34 +0900

    feat: 아메리카노 버튼 클릭 로직에 leading edge 적용

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const useHandleDispenser = (timeInterval) => {
   const [dispenserAction, setDispenserAction] = useState([]);
-  const handleDispenser = async (ingredient) => {
+  const handleDispenser = async (ingredient, timeId) => {
     return new Promise((resolve) => {
-      setTimeout(() => {
+      timeId.current = setTimeout(() => {
         setDispenserAction((prev) => [...prev, ingredient]);
         resolve();
       }, timeInterval);
@@ -17,18 +17,25 @@ const useHandleDispenser = (timeInterval) => {
 function App() {
   const { dispenserAction, handleDispenser, setDispenserAction } =
     useHandleDispenser(500);
-  const handleClickAmericanoButton = async () => {
+  const timeId = useRef(null);
+
+  const makeAmericano = async () => {
+    if (timeId.current) return;
     setDispenserAction([]);
-    await handleDispenser("컵");
-    await handleDispenser("에스프레소");
-    await handleDispenser("뜨거운 물");
+    await handleDispenser("컵", timeId);
+    await handleDispenser("에스프레소", timeId);
+    await handleDispenser("뜨거운 물", timeId);
+    timeId.current = null;
   };
 
   return (
     <div className="App">
-      <button type="button" onClick={handleClickAmericanoButton}>
+      <h1>자판기</h1>
+      <h2>자판기 버튼</h2>
+      <button type="button" onClick={makeAmericano}>
         아메리카노
       </button>
+      <h2>디스펜서</h2>
       {dispenserAction.map((action) => (
         <p key={action}>{action}</p>
       ))}
