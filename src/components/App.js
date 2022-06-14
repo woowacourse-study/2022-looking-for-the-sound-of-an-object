880f0a2add09a4410e936ba34092c45d7b68d7ae
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-06-14 17:46:16 +0900

    feat: 충분한 돈을 투입하면, 해당하는 자판기 버튼 활성화
    누적 투입 금액에 따라 활성화

--- src/components/App.js
+++ src/components/App.js
@@ -15,15 +15,27 @@ const useHandleDispenser = (timeInterval) => {
 };
 
 function App() {
+  const [inputMoney, setInputMoney] = useState(0);
   const { dispenserAction, handleDispenser, setDispenserAction } =
     useHandleDispenser(500);
+  const insertedMoney = useRef(null);
   const timeId = useRef(null);
+
+  const InsertMoney = () => {
+    const insert = insertedMoney.current.valueAsNumber;
+    if (inputMoney + insert >= 50000) {
+      alert("더이상 금액을 넣을 수 없습니다(최대 5만원).");
+      return;
+    }
+    setInputMoney((prev) => prev + insert);
+  };
 
   const makeMilk = async () => {
     if (timeId.current) return;
     setDispenserAction([]);
     await handleDispenser("컵", timeId);
     await handleDispenser("우유", timeId);
+    await handleDispenser("우유 나왔습니다~ :D", timeId);
     timeId.current = null;
   };
 
@@ -32,6 +44,7 @@ function App() {
     setDispenserAction([]);
     await handleDispenser("컵", timeId);
     await handleDispenser("에스프레소", timeId);
+    await handleDispenser("에스프레소 나왔습니다~ :D", timeId);
     timeId.current = null;
   };
 
@@ -41,6 +54,7 @@ function App() {
     await handleDispenser("컵", timeId);
     await handleDispenser("에스프레소", timeId);
     await handleDispenser("뜨거운 물", timeId);
+    await handleDispenser("아메리카노 나왔습니다~ :D", timeId);
     timeId.current = null;
   };
 
@@ -50,29 +64,67 @@ function App() {
     await handleDispenser("컵", timeId);
     await handleDispenser("우유", timeId);
     await handleDispenser("에스프레소", timeId);
+    await handleDispenser("카페라떼 나왔습니다~ :D", timeId);
     timeId.current = null;
   };
 
   return (
     <div className="App">
       <h1>자판기</h1>
-      <h2>자판기 버튼</h2>
-      <button type="button" onClick={makeMilk}>
-        우유
-      </button>
-      <button type="button" onClick={makeEspresso}>
-        에스프레소
-      </button>
-      <button type="button" onClick={makeAmericano}>
-        아메리카노
-      </button>
-      <button type="button" onClick={makeCafeLatte}>
-        카페라떼
-      </button>
-      <h2>디스펜서</h2>
-      {dispenserAction.map((action) => (
-        <p key={action}>{action}</p>
-      ))}
+      <section>
+        <h2>금액 투입구</h2>
+        <label htmlFor="insert">금액을 투입해 주세요.</label>
+        <br />
+        <input
+          id="insert"
+          type="number"
+          max={50000}
+          min={100}
+          step={100}
+          placeholder="1,000"
+          ref={insertedMoney}
+        />
+        원
+        <button type="button" onClick={InsertMoney}>
+          투입하기
+        </button>
+        <p>
+          투입한 금액: <span>{inputMoney.toLocaleString()}</span>원
+        </p>
+      </section>
+      <section>
+        <h2>자판기 버튼</h2>
+        <button type="button" onClick={makeMilk} disabled={inputMoney < 1000}>
+          우유
+        </button>
+        <button
+          type="button"
+          onClick={makeEspresso}
+          disabled={inputMoney < 1000}
+        >
+          에스프레소
+        </button>
+        <button
+          type="button"
+          onClick={makeAmericano}
+          disabled={inputMoney < 1500}
+        >
+          아메리카노
+        </button>
+        <button
+          type="button"
+          onClick={makeCafeLatte}
+          disabled={inputMoney < 2000}
+        >
+          카페라떼
+        </button>
+      </section>
+      <section>
+        <h2>디스펜서</h2>
+        {dispenserAction.map((action) => (
+          <p key={action}>{action}</p>
+        ))}
+      </section>
     </div>
   );
 }
