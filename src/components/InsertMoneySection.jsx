0a03e4ca90934c8c2b4f9bb2e0ba9e2b7c823cb3
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-06-14 20:28:10 +0900

    fix: 금액 투입시 유효성 검증 로직 추가

--- src/components/InsertMoneySection.jsx
+++ src/components/InsertMoneySection.jsx
@@ -1,12 +1,32 @@
 import { useRef } from "react";
 
-import { INSERT_MONEY_RANGE } from "../constants";
+import { COIN_UNIT, INSERT_MONEY_RANGE } from "../constants";
+
+const validateInsertMoney = (insert) => {
+  if (!insert) {
+    throw new Error("금액을 입력해주세요.");
+  }
+  if (insert < INSERT_MONEY_RANGE.MIN || insert > INSERT_MONEY_RANGE.MAX) {
+    throw new Error(
+      `${INSERT_MONEY_RANGE.MIN}원 이상, ${INSERT_MONEY_RANGE.MAX}원 이하의 금액만 입력할 수 있습니다.`
+    );
+  }
+  if (insert % COIN_UNIT[10] !== 0) {
+    throw new Error(`${COIN_UNIT[10]}원 단위만 입력이 가능합니다.`);
+  }
+};
 
 function InsertMoneySection({ inputMoney, setInputMoney }) {
   const insertMoneyRef = useRef(null);
 
   const InsertMoney = () => {
     const insert = insertMoneyRef.current.valueAsNumber;
+    try {
+      validateInsertMoney(insert);
+    } catch (error) {
+      alert(error.message);
+      return;
+    }
     if (inputMoney + insert >= INSERT_MONEY_RANGE.MAX) {
       alert(
         `더이상 금액을 넣을 수 없습니다(최대 ${INSERT_MONEY_RANGE.MAX.toLocaleString()}원).`
@@ -26,7 +46,7 @@ function InsertMoneySection({ inputMoney, setInputMoney }) {
         type="number"
         max={INSERT_MONEY_RANGE.MAX}
         min={INSERT_MONEY_RANGE.MIN}
-        step={100}
+        step={COIN_UNIT[10]}
         placeholder="1,000"
         ref={insertMoneyRef}
       />
