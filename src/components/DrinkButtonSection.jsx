354f41b79566bc53bec4d319ac4f75fdd8ed2d4b
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-06-14 23:13:43 +0900

    feat: 여러 음료 구입 기능 구현
    음료 버튼을 눌러 구입 목록에 담기
    환불하기 버튼을 눌러 환불
    음료 받기 버튼을 눌러 디스펜서에 음료 출력
    음료를 받은 후에는 환불 불가능

--- src/components/DrinkButtonSection.jsx
+++ src/components/DrinkButtonSection.jsx
@@ -10,42 +10,53 @@ function DrinkButtonSection({
   handleDispenser,
   setInputMoney,
 }) {
-  const [latestDrink, setLatestDrink] = useState("");
+  const [latestDrinks, setLatestDrinks] = useState([]);
   const timeId = useRef(null);
 
+  const addDrinkToList = (drink) => () => {
+    const drinkPrice = DRINK[drink].PRICE;
+    if (drinkPrice > inputMoney) {
+      alert("더 이상 구입할 수 없습니다.");
+      return;
+    }
+    setInputMoney((prev) => prev - drinkPrice);
+    setLatestDrinks((prev) => [...prev, drink]);
+  };
+
+  const buyDrinkList = async () => {
+    if (timeId.current || !latestDrinks.length) return;
+    if (
+      window.confirm(`음료가 나오면 환불할 수 없습니다. 음료를 받으시겠습니까?`)
+    ) {
+      setDispenserAction([]);
+      for (const drink of latestDrinks) {
+        await makeDrink(DRINK[drink].ACTION);
+        timeId.current = null;
+      }
+      setLatestDrinks([]);
+    }
+  };
+
   const makeDrink = async (dispenserActionList) => {
-    setDispenserAction([]);
     for (const action of dispenserActionList) {
       await handleDispenser(action, timeId);
     }
   };
 
-  const buyDrink = (drink) => async () => {
-    if (timeId.current) return;
-    if (
-      window.confirm(
-        `${drink}를 구매하시는 게 맞나요? 음료가 나온 후에 환불할 수 있습니다.`
-      )
-    ) {
-      setLatestDrink(DRINK[drink].NAME);
-      setInputMoney((prev) => prev - DRINK[drink].PRICE);
-      await makeDrink(DRINK[drink].ACTION);
-      timeId.current = null;
-    }
-  };
-
   const refundMoney = () => {
     if (timeId.current) {
-      alert("음료가 나온 후에 환불할 수 있습니다.");
+      alert("음료가 나온 후에는 환불할 수 없습니다.");
       return;
     }
-    if (
-      latestDrink &&
-      window.confirm(`정말 ${latestDrink}를 환불하시겠습니까?`)
-    ) {
+    if (!latestDrinks.length) return;
+    if (window.confirm(`정말 구입한 음료(들)를 환불하시겠습니까?`)) {
+      const refundMoney = latestDrinks.reduce(
+        (acc, drink) => acc + DRINK[drink].PRICE,
+        0
+      );
+      setInputMoney((prev) => prev + refundMoney);
       setDispenserAction([]);
-      setLatestDrink("");
-      setInputMoney((prev) => prev + DRINK[latestDrink].PRICE);
+      setLatestDrinks([]);
     }
   };
 
@@ -53,32 +64,43 @@ function DrinkButtonSection({
     <section>
       <h2>자판기 버튼</h2>
       <DrinkButton
-        onClick={buyDrink(DRINK.MILK.NAME)}
+        onClick={addDrinkToList(DRINK.MILK.NAME)}
         disabled={inputMoney < DRINK.MILK.PRICE}
       >
         우유
       </DrinkButton>
       <DrinkButton
-        onClick={buyDrink(DRINK.ESPRESSO.NAME)}
+        onClick={addDrinkToList(DRINK.ESPRESSO.NAME)}
         disabled={inputMoney < DRINK.ESPRESSO.PRICE}
       >
         에스프레소
       </DrinkButton>
       <DrinkButton
-        onClick={buyDrink(DRINK.AMERICANO.NAME)}
+        onClick={addDrinkToList(DRINK.AMERICANO.NAME)}
         disabled={inputMoney < DRINK.AMERICANO.PRICE}
       >
         아메리카노
       </DrinkButton>
       <DrinkButton
-        onClick={buyDrink(DRINK.CAFELATTE.NAME)}
+        onClick={addDrinkToList(DRINK.CAFELATTE.NAME)}
         disabled={inputMoney < DRINK.CAFELATTE.PRICE}
       >
         카페라떼
       </DrinkButton>
-      <button type="button" onClick={refundMoney} disabled={!latestDrink}>
+      <button
+        type="button"
+        onClick={refundMoney}
+        disabled={!latestDrinks.length}
+      >
         환불하기
       </button>
+      <section>
+        <h3>구입한 목록</h3>
+        <p>{latestDrinks.join(", ")}</p>
+        <button type="button" onClick={buyDrinkList}>
+          음료 받기
+        </button>
+      </section>
     </section>
   );
 }
