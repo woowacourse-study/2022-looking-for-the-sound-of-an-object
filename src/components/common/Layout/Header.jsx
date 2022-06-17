8a230374124725495584234fceab868df995db08
JASUN LEE <liswktjs@gmail.com>
2022-06-17 16:06:28 +0900

    feat: Header 컴포넌트 구현

---
+++ src/components/common/Layout/Header.jsx
@@ -0,0 +1,48 @@
+import { Outlet, useNavigate } from "react-router";
+import styled from "styled-components";
+import { COLORS } from "../../../style/theme";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  width: 100%;
+  height: fit-content;
+  justify-content: space-around;
+  background-color: ${COLORS.BLUE};
+`;
+
+const HeaderTitle = styled.h2`
+  padding: 0;
+  margin: 0.5rem 0;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  gap: 1rem;
+  margin-top: 0.7rem;
+`;
+
+const NavButton = styled.a`
+  text-decoration: none;
+  &:hover {
+    color: ${COLORS.WHITE};
+  }
+`;
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <HeaderContainer>
+        <HeaderTitle>자판기</HeaderTitle>
+        <Nav>
+          <NavButton onClick={navigate("/store")}>재료충전</NavButton>
+          <NavButton onClick={navigate("/")}>상품구매</NavButton>
+        </Nav>
+      </HeaderContainer>
+      <Outlet />
+    </>
+  );
+};
+
+export default Header;
