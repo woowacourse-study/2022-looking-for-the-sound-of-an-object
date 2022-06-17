1e2f910266151f02ac77594513b137319d3bd39d
JASUN LEE <liswktjs@gmail.com>
2022-06-17 17:21:51 +0900

    style: MenuSelector 컴포넌트 스타일링

---
+++ src/components/pages/VendingMachine/MenuSelect.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import Button from "../../common/Button";
+
+const MenuSelectContainer = styled.article`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  margin-bottom: 2rem;
+`;
+
+const MenuSelect = () => {
+  return (
+    <MenuSelectContainer>
+      <h2 hidden>메뉴 고르는 곳</h2>
+      <label>
+        <Button width="5rem">에스프레소</Button>
+        1000원
+      </label>
+      <label>
+        <Button width="5rem">아메리카노</Button>
+        1000원
+      </label>
+      <label>
+        <Button width="5rem">카페라떼</Button>
+        1000원
+      </label>
+      <label>
+        <Button width="5rem">우유</Button>
+        1000원
+      </label>
+    </MenuSelectContainer>
+  );
+};
+
+export default MenuSelect;
