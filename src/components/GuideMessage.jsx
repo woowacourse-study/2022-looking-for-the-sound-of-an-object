5e4e09e78cfbc30a4dd592ebe9d8712f3fc386fe
soyi47 <soyi.dev@gmail.com>
2022-06-21 00:12:05 +0900

    refactor: useMemo를 이용한 GuideMessage 로직 간소화
    - 컴포넌트 내부 guideText 상태와 useEffect 제거
    - useMemo를 사용하여, 주문 상태와 메뉴를 의존성 배열 요소로 가지는 변수 guideText를 새롭게 정의

--- src/components/GuideMessage.jsx
+++ src/components/GuideMessage.jsx
@@ -1,50 +1,23 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useMemo } from "react";
 
 import { ORDER_PROGRESS } from "../constants";
 import menus from "./../constants/menus";
 
-const GuideMessage = ({ customerCharge, order }) => {
-  const [guideText, setGuideText] = useState("");
-
-  const updateOnOrderPending = useCallback(() => {
-    setGuideText(
-      customerCharge.value >=
-        Math.min(...Object.keys(menus).map((menu) => menus[menu].price))
-        ? "원하는 음료를 선택하세요."
-        : "투입 금액이 부족하여 선택 가능한 음료가 없습니다."
-    );
-  }, [customerCharge.value]);
-
-  const updateOnOrderMaking = (menu) => {
-    setGuideText(`${menus[menu].name} 준비 중...`);
-  };
-
-  const updateOnOrderComplete = (menu) => {
-    setGuideText(`${menus[menu].name} 나왔습니다. 😉`);
-  };
-
-  useEffect(() => {
-    const { progress, orderedMenu } = order;
-
+const GuideMessage = ({ customerCharge, order: { progress, orderedMenu } }) => {
+  const guideText = useMemo(() => {
     switch (progress) {
       case ORDER_PROGRESS.PENDING:
-        updateOnOrderPending();
-        break;
+        return customerCharge.value >=
+          Math.min(...Object.keys(menus).map((menu) => menus[menu].price))
+          ? "원하는 음료를 선택하세요."
+          : "투입 금액이 부족하여 선택 가능한 음료가 없습니다.";
       case ORDER_PROGRESS.MAKING:
-        updateOnOrderMaking(orderedMenu);
-        break;
+        return `${menus[orderedMenu].name} 준비 중...`;
       case ORDER_PROGRESS.COMPLETE:
-        updateOnOrderComplete(orderedMenu);
-        break;
+        return `${menus[orderedMenu].name} 나왔습니다. 😉`;
       default:
     }
-  }, [order, updateOnOrderPending]);
-
-  useEffect(() => {
-    if (order.progress === ORDER_PROGRESS.PENDING) {
-      updateOnOrderPending();
-    }
-  }, [order.progress, updateOnOrderPending]);
+  }, [customerCharge.value, orderedMenu, progress]);
 
   return <p className="guide text">{guideText}</p>;
 };
