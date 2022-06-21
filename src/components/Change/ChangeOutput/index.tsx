84b95257ac0d793eb6c4cd42b0721dc7bd38f940
이아현 <lah1203@naver.com>
2022-06-21 12:00:21 +0900

    feat: 음료 제조 중에 반환 버튼 클릭시 막는 기능 추가

--- src/components/Change/ChangeOutput/index.tsx
+++ src/components/Change/ChangeOutput/index.tsx
@@ -1,14 +1,22 @@
-import { useRecoilState, useSetRecoilState } from 'recoil';
-import { changeState, guideState } from 'recoil/states';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { changeState, dispenserState, guideState } from 'recoil/states';
 import { formatNumber } from 'utils/formatter';
+
+import { DISPENSER_STATE } from 'constants/dispenser';
 
 import * as S from './index.styled';
 
 function ChangeOutput() {
   const setGuideMessage = useSetRecoilState(guideState);
   const [change, setChange] = useRecoilState(changeState);
+  const dispenser = useRecoilValue(dispenserState);
 
   const returnChange = () => {
+    if (dispenser === DISPENSER_STATE.MAKING) {
+      alert('음료 제조가 끝난 후 반환해주세요.');
+      return;
+    }
+
     if (!window.confirm('반환하시겠습니까?')) return;
 
     setChange(0);
