f7c9c354310021143fec9601f8b1db29df853fbf
이아현 <lah1203@naver.com>
2022-06-21 11:51:19 +0900

    feat: 잔돈 충전 input에 ref 추가
    - 잔돈 충전 성공 시 input value 비우기
    - 잔돈 충전 성공 시 input에 focus

--- src/components/Change/ChangeInput/index.tsx
+++ src/components/Change/ChangeInput/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import { useSetRecoilState } from 'recoil';
 import { changeState, guideState } from 'recoil/states';
@@ -9,6 +9,7 @@ import * as S from './index.styled';
 function ChangeInput() {
   const setGuideMessage = useSetRecoilState(guideState);
   const setChange = useSetRecoilState(changeState);
+  const changeRef = useRef<HTMLInputElement>(null);
 
   const chargeChange = (e: React.FormEvent) => {
     e.preventDefault();
@@ -31,11 +32,16 @@ function ChangeInput() {
     setChange(prevState => prevState + money);
 
     setGuideMessage('투입되었습니다.');
+
+    if (!changeRef.current) return;
+
+    changeRef.current.value = '';
+    changeRef.current.focus();
   };
 
   return (
     <S.ChangeInputForm onSubmit={chargeChange}>
-      <input type="number" />
+      <input type="number" ref={changeRef} />
       <button>투입</button>
     </S.ChangeInputForm>
   );
