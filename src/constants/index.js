551984ed3336cfbe8544e2bee5e77d1a65a0b079
JASUN LEE <liswktjs@gmail.com>
2022-06-25 23:13:52 +0900

    feat: redux에  콜라, 사이다 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,4 +3,15 @@ export const MENU_PRICE = {
   americano: 1500,
   cafelatte: 2000,
   milk: 1500,
+  coke: 1500,
+  sida: 1500,
+};
+
+export const MENU_NAME = {
+  espresso: "espresso",
+  americano: "americano",
+  cafelatte: "cafelatte",
+  milk: "milk",
+  coke: "coke",
+  sida: "sida",
 };
