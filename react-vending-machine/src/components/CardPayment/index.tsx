21e0eba251585fd5965459d001c1de3097ebb6b5
prefer2 <sunho6200@gmail.com>
2022-07-06 13:48:32 +0900

    feat: CardPayment component 구현

---
+++ react-vending-machine/src/components/CardPayment/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import { STATUS } from "../../constants";
+
+interface CardPaymentType {
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  vendingMachineState: string;
+}
+
+const StyledCardPayment = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+
+  border-radius: 4px;
+  color: ${({ theme }) => theme.color.white};
+  background-color: ${({ theme }) => theme.color.black};
+`;
+
+const StyledCardTag = styled.button`
+  width: 100px;
+  height: 60px;
+  margin-top: 5px;
+
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.color.yellow};
+`;
+
+export default function CardPayment({
+  onClick,
+  vendingMachineState,
+}: CardPaymentType) {
+  return (
+    <StyledCardPayment>
+      <div>카드를 태그해주세요</div>
+      <StyledCardTag
+        onClick={onClick}
+        disabled={vendingMachineState !== STATUS.READY}
+      >
+        태그하기
+      </StyledCardTag>
+    </StyledCardPayment>
+  );
+}
