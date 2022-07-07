108fa9b0a1c313962d4e9810b72b0c8a9cc8de56
soyi47 <soyi.dev@gmail.com>
2022-07-07 15:53:24 +0900

    feat: CardPaymentSection 컴포넌트 생성

---
+++ src/components/PaymentSection/CardPaymentSection.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CardPaymentSection = () => {
+  return (
+    <section className="payment-area">
+      <h3>카드 결제</h3>
+      <button className="card-payment-button">💳 카드 결제</button>
+    </section>
+  );
+};
+
+export default CardPaymentSection;
