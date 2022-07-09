84a4c88adb6af9f87002c999e27cbeb68c0d7a31
이아현 <lah1203@naver.com>
2022-07-09 11:26:02 +0900

    feat: 잔돈 반환 기능 구현

--- src/components/Change/ChangeOutput/index.tsx
+++ src/components/Change/ChangeOutput/index.tsx
@@ -1,15 +1,46 @@
+import { useState } from 'react';
+
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { changeState, dispenserState, guideState } from 'recoil/states';
+import { Coin, ReturnCoin } from 'types';
 import { formatNumber } from 'utils/formatter';
 
 import { DISPENSER_STATE } from 'constants/dispenser';
 
 import * as S from './index.styled';
 
+const returnCoinsInitialState = {
+  500: 0,
+  100: 0,
+  50: 0,
+  10: 0,
+};
+
 function ChangeOutput() {
   const setGuideMessage = useSetRecoilState(guideState);
   const [change, setChange] = useRecoilState(changeState);
   const dispenser = useRecoilValue(dispenserState);
+  const [returnCoins, setReturnCoins] = useState<ReturnCoin>(
+    returnCoinsInitialState
+  );
+
+  const makeReturnCoins = () => {
+    const newReturnCoins = Object.assign(returnCoinsInitialState);
+
+    (Object.keys(returnCoins) as unknown as Coin[])
+      .reverse()
+      .forEach(coinType => {
+        setChange(prevState => {
+          newReturnCoins[coinType] = parseInt(
+            (prevState / coinType).toString(),
+            10
+          );
+          return prevState % coinType;
+        });
+      });
+
+    setReturnCoins(newReturnCoins);
+  };
 
   const returnChange = () => {
     if (dispenser === DISPENSER_STATE.MAKING) {
@@ -19,6 +50,7 @@ function ChangeOutput() {
 
     if (!window.confirm('반환하시겠습니까?')) return;
 
+    makeReturnCoins();
     setChange(0);
 
     setGuideMessage('반환되었습니다.');
@@ -30,6 +62,18 @@ function ChangeOutput() {
       <S.ReturnButton type="button" onClick={returnChange}>
         반환
       </S.ReturnButton>
+      <S.ReturnCoinTable>
+        <tbody>
+          {(Object.keys(returnCoins).reverse() as unknown as Coin[]).map(
+            coinType => (
+              <tr key={coinType}>
+                <td>{coinType}원</td>
+                <td>{returnCoins[coinType]}개</td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </S.ReturnCoinTable>
     </S.ChangeContainer>
   );
 }
--- src/constants/rules.ts
+++ src/constants/rules.ts
@@ -1,7 +1,7 @@
 const CHANGE_RULES = {
   MIN: 500,
   MAX: 999999500,
-  UNIT: 500,
+  UNIT: 10,
 };
 
 export { CHANGE_RULES };
