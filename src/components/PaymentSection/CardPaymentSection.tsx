5b3a7bed35d34a571063dda2b7f986e1aa56b92f
soyi47 <soyi.dev@gmail.com>
2022-07-09 15:23:55 +0900

    feat: 카드 결제 버튼 클릭 이벤트 핸들러 추가

--- src/components/PaymentSection/CardPaymentSection.tsx
+++ src/components/PaymentSection/CardPaymentSection.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { usePayment } from "../../context/PaymentContext";
 
 const CardPaymentSection = () => {
+  const { isOnCardPayment, setIsOnCardPayment } = usePayment();
+
   return (
     <section className="payment-area">
       <h3>카드 결제</h3>
-      <button className="card-payment-button">💳 카드 결제</button>
+      <button
+        className="card-payment-button"
+        onClick={() => {
+          setIsOnCardPayment((prevState: boolean) => !prevState);
+        }}
+      >
+        {isOnCardPayment ? "💳 카드 결제 취소" : "💳 카드 결제"}
+      </button>
     </section>
   );
 };
