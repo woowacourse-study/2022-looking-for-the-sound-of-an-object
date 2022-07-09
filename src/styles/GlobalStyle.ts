60f345b94c9d9ab611871ad4c52e1d96f39f9778
이아현 <lah1203@naver.com>
2022-07-09 11:26:35 +0900

    style: 폰트 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -10,12 +10,20 @@ const GlobalStyle = createGlobalStyle`
     align-items: center;
 
     font-size: 16px;
+    font-family: 'MapoPeacefull', sans-serif;
   }
 
   button {
     border: none;
 
     cursor: pointer;
+  }
+
+  @font-face {
+    font-family: 'MapoPeacefull';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/MapoPeacefullA.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
   }
 `;
 
