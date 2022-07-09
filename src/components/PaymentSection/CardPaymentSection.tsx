f5ea1c11b2fb4ebec4ac8f010ff3683a733d0bf7
soyi47 <soyi.dev@gmail.com>
2022-07-09 15:52:32 +0900

    feat: 충전된 금액이 있는 경우, 카드 결제로 전환하지 못 하도록 로직 추가

--- src/components/PaymentSection/CardPaymentSection.tsx
+++ src/components/PaymentSection/CardPaymentSection.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import { usePayment } from "../../context/PaymentContext";
 
 const CardPaymentSection = () => {
-  const { isOnCardPayment, setIsOnCardPayment } = usePayment();
+  const { isOnCardPayment, setIsOnCardPayment, customerCharge } = usePayment();
 
   return (
     <section className="payment-area">
-      <h3>카드 결제</h3>
+      <h3 className="sr-only">카드 결제</h3>
       <button
         className="card-payment-button"
         onClick={() => {
+          if (customerCharge.value) {
+            alert(
+              "충전된 금액이 있습니다.\n충전금을 반환 받은 후 카드 결제를 시도해주세요."
+            );
+            return;
+          }
           setIsOnCardPayment((prevState: boolean) => !prevState);
         }}
       >
